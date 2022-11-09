1d4ddbf21b9c13ab3fe11888615fbf45c7f41578
Dhruv Mangal <dhruv.mangal@hiteshi.com>
2022-11-09 20:40:16 +0530

    unknown changes made by ishan form dhruv's pc

---
+++ utils/mySqlOperations/model/modelDef.js
@@ -0,0 +1,13 @@
+/*
+Created on 9-11-22
+Created By Dhruv Mangal
+
+*/
+
+var MySqlModel = new Object();
+MySqlModel.initialize = (data)=>{
+    for(variable in data){
+        if(variable && variable.name && variable.type && true ){}
+    }
+    this.model = model
+}
